7d575054d72c9dc2c35d25155e59918bf59271e5
hyunn12 <hyunn964@gmail.com>
2025-07-15 21:57:38 +0900

    [feat] user 회원가입 E2E 테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserControllerE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserControllerE2ETest.java
@@ -1,6 +1,5 @@
 package com.loopers.interfaces.api.user;
 
-import com.loopers.infrastructure.user.UserJpaRepository;
 import com.loopers.interfaces.api.ApiResponse;
 import com.loopers.utils.DatabaseCleanUp;
 import org.junit.jupiter.api.AfterEach;
@@ -13,6 +12,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,17 +24,14 @@ class UserControllerE2ETest {
 
     private final TestRestTemplate testRestTemplate;
     private final DatabaseCleanUp databaseCleanUp;
-    private final UserJpaRepository userJpaRepository;
 
     @Autowired
     public UserControllerE2ETest(
             TestRestTemplate testRestTemplate,
-            DatabaseCleanUp databaseCleanUp,
-            UserJpaRepository userJpaRepository
+            DatabaseCleanUp databaseCleanUp
     ) {
         this.testRestTemplate = testRestTemplate;
         this.databaseCleanUp = databaseCleanUp;
-        this.userJpaRepository = userJpaRepository;
     }
 
     @AfterEach
@@ -45,12 +42,12 @@ class UserControllerE2ETest {
     @DisplayName("POST /api/v1/users")
     @Nested
     class POST {
+        private final String requestUrl = "/api/v1/users";
 
         @DisplayName("API 호출 테스트")
         @Test
         void callApi() {
             // arrange
-            String requestUrl = "/api/v1/users";
             UserDto.JoinRequest requestBody = new UserDto.JoinRequest(
                     "test123",
                     "test@test.com",
@@ -72,6 +69,58 @@ class UserControllerE2ETest {
                     () -> assertThat(response.getBody().data().birth()).isEqualTo(requestBody.birth())
             );
         }
+
+        @DisplayName("회원가입 성공 시 생성된 유저 정보를 응답으로 반환함")
+        @Test
+        void joinSuccess() {
+            // arrange
+            UserDto.JoinRequest requestBody = new UserDto.JoinRequest(
+                    "test123",
+                    "test@test.com",
+                    "F",
+                    "2000-01-01"
+            );
+
+            // act
+            ParameterizedTypeReference<ApiResponse<UserDto.UserResponse>> responseType = new ParameterizedTypeReference<>() {};
+            ResponseEntity<ApiResponse<UserDto.UserResponse>> response =
+                    testRestTemplate.exchange(requestUrl, HttpMethod.POST, new HttpEntity<>(requestBody), responseType);
+
+            // assert
+            assertAll(
+                    () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                    () -> assertThat(response.getBody().data().userId()).isEqualTo(requestBody.userId()),
+                    () -> assertThat(response.getBody().data().email()).isEqualTo(requestBody.email()),
+                    () -> assertThat(response.getBody().data().gender()).isEqualTo(requestBody.gender()),
+                    () -> assertThat(response.getBody().data().birth()).isEqualTo(requestBody.birth())
+            );
+        }
+
+        @DisplayName("회원가입 시 성별이 없을 경우 400 Bad Request 응답을 반환함")
+        @Test
+        void joinWithoutGender_returnBadRequest() {
+            // arrange
+            UserDto.JoinRequest requestBody = new UserDto.JoinRequest(
+                    "test123",
+                    "test@test.com",
+                    null,
+                    "2000-01-01"
+            );
+
+            // act
+            ParameterizedTypeReference<ApiResponse<UserDto.UserResponse>> responseType = new ParameterizedTypeReference<>() {};
+            ResponseEntity<ApiResponse<UserDto.UserResponse>> response =
+                    testRestTemplate.exchange(requestUrl, HttpMethod.POST, new HttpEntity<>(requestBody), responseType);
+
+            // assert
+            assertAll(
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST),
+                    () -> assertThat(response.getBody()).isNotNull(),
+                    () -> assertThat(response.getBody().meta().result()).isEqualTo(ApiResponse.Metadata.Result.FAIL),
+                    () -> assertThat(response.getBody().meta().message()).contains("성별 형식이 잘못되었습니다.")
+            );
+        }
+
     }
 
     @DisplayName("GET /api/v1/users/me")
