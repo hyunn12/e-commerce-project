8d356b08340a32c02b3511230665da2c8d9b35bb
hyunn12 <hyunn964@gmail.com>
2025-08-19 23:45:10 +0900

    chore: 누락 annotation 추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductV1Controller.java
@@ -2,6 +2,7 @@ package com.loopers.interfaces.api.product;
 
 import com.loopers.application.product.ProductFacade;
 import com.loopers.interfaces.api.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,7 @@ public class ProductV1Controller implements ProductV1ApiSpec {
 
     @GetMapping
     public ApiResponse<ProductV1Dto.ProductResponse.Summary> getList(
-         ProductV1Dto.ProductRequest.Summary request
+            @Valid ProductV1Dto.ProductRequest.Summary request
     ) {
         return ApiResponse.success(ProductV1Dto.ProductResponse.Summary.from(productFacade.getList(request.toCommand())));
     }
