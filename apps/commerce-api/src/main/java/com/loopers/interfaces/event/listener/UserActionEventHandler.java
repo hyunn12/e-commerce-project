f6f88d85baa81628faa07a86f16fa76a1438f96f
hyunn12 <hyunn964@gmail.com>
2025-08-29 14:23:05 +0900

    chore: 누락 annotation 추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/event/listener/UserActionEventHandler.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/event/listener/UserActionEventHandler.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -19,24 +21,28 @@ public class UserActionEventHandler {
     private final ExternalUserActionSender externalUserActionSender;
 
     @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleLikeAdded(LikeAddEvent event) {
         externalUserActionSender.send(UserActionEvent.of(event.getUserId(), LIKE_ADD, event.getProductId()));
     }
 
     @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleLikeDeleted(LikeDeleteEvent event) {
         externalUserActionSender.send(UserActionEvent.of(event.getUserId(), LIKE_DELETE, event.getProductId()));
     }
 
     @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleOrderCreated(OrderCreatedEvent event) {
         externalUserActionSender.send(UserActionEvent.of(event.getUserId(), ORDER, event.getOrderId()));
     }
 
     @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleProductViewed(ProductViewEvent event) {
         externalUserActionSender.send(UserActionEvent.of(event.getUserId(), VIEW, event.getProductId()));
